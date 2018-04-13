62880e3135df6eb916fa55ab948c71bed0d34223
Gabriel Kling <gabrielkling@outlook.com>
2018-04-12 21:38:33 -0300

    Mudança de cores

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 document.getElementById('form').addEventListener('submit', pegarDados);
 
-function pegarDados(e) {
+limparCampos();
+
+function pegarDados(e){
 
   var valor1 = document.getElementById('valor1').value;
 
@@ -12,148 +14,154 @@ function pegarDados(e) {
 
   var valorConvertido = document.getElementById('valor2');
 
-  if (from == para) {
+  if (from == para){
     valorConvertido.value = valor1;
-  } else if (from == "miliMetro") {
-    if (para == "centiMetro") {
-      valorConvertido.value = valor1 / 10;
-    }
-    if (para == "deciMetro") {
-      valorConvertido.value = valor1 / 100;
-    }
-    if (para == "metro") {
-      valorConvertido.value = valor1 / 1000;
-    }
-    if (para == "decaMetro") {
-      valorConvertido.value = valor1 / 10000;
-    }
-    if (para == "hectMetro") {
-      valorConvertido.value = valor1 / 100000;
-    }
-    if (para == "kiloMetro") {
-      valorConvertido.value = valor1 / 1000000;
-    }
-  } else if (from == "centiMetro") {
-    if (para == "miliMetro") {
-      valorConvertido.value = valor1 * 10;
-    }
-    if (para == "deciMetro") {
-      valorConvertido.value = valor1 / 10;
-    }
-    if (para == "metro") {
-      valorConvertido.value = valor1 / 100;
-    }
-    if (para == "decaMetro") {
-      valorConvertido.value = valor1 / 1000;
-    }
-    if (para == "hectMetro") {
-      valorConvertido.value = valor1 / 10000;
-    }
-    if (para == "kiloMetro") {
-      valorConvertido.value = valor1 / 100000;
-    }
-  } else if (from == "deciMetro") {
-    if (para == "miliMetro") {
-      valorConvertido.value = valor1 * 100;
-    }
-    if (para == "centiMetro") {
-      valorConvertido.value = valor1 * 10;
-    }
-    if (para == "metro") {
-      valorConvertido.value = valor1 / 10;
-    }
-    if (para == "decaMetro") {
-      valorConvertido.value = valor1 / 100;
-    }
-    if (para == "hectMetro") {
-      valorConvertido.value = valor1 / 1000;
-    }
-    if (para == "kiloMetro") {
-      valorConvertido.value = valor1 / 10000;
-    }
-  } else if (from == "metro") {
-    if (para == "miliMetro") {
-      valorConvertido.value = valor1 * 1000;
-    }
-    if (para == "centiMetro") {
-      valorConvertido.value = valor1 * 100;
-    }
-    if (para == "deciMetro") {
-      valorConvertido.value = valor1 * 10;
-    }
-    if (para == "decaMetro") {
-      valorConvertido.value = valor1 / 10;
-    }
-    if (para == "hectMetro") {
-      valorConvertido.value = valor1 / 100;
-    }
-    if (para == "kiloMetro") {
-      valorConvertido.value = valor1 / 1000;
-    }
-  } else if (from == "decaMetro") {
-    if (para == "miliMetro") {
-      valorConvertido.value = valor1 * 10000;
-    }
-    if (para == "centiMetro") {
-      valorConvertido.value = valor1 * 1000;
-    }
-    if (para == "deciMetro") {
-      valorConvertido.value = valor1 * 100;
-    }
-    if (para == "metro") {
-      valorConvertido.value = valor1 * 10;
-    }
-    if (para == "hectMetro") {
-      valorConvertido.value = valor1 / 10;
-    }
-    if (para == "kiloMetro") {
-      valorConvertido.value = valor1 / 100;
-    }
-  } else if (from == "hectMetro") {
-    if (para == "miliMetro") {
-      valorConvertido.value = valor1 * 100000;
-    }
-    if (para == "centiMetro") {
-      valorConvertido.value = valor1 * 10000;
-    }
-    if (para == "deciMetro") {
-      valorConvertido.value = valor1 * 1000;
-    }
-    if (para == "metro") {
-      valorConvertido.value = valor1 * 100;
-    }
-    if (para == "decaMetro") {
-      valorConvertido.value = valor1 * 10;
-    }
-    if (para == "kiloMetro") {
-      valorConvertido.value = valor1 / 10;
-    }
-  } else if (from == "kiloMetro") {
-    if (para == "miliMetro") {
-      valorConvertido.value = valor1 * 1000000;
-    }
-    if (para == "centiMetro") {
-      valorConvertido.value = valor1 * 100000;
-    }
-    if (para == "deciMetro") {
-      valorConvertido.value = valor1 * 10000;
-    }
-    if (para == "metro") {
-      valorConvertido.value = valor1 * 1000;
-    }
-    if (para == "decaMetro") {
-      valorConvertido.value = valor1 * 100;
-    }
-    if (para == "hectMetro") {
-      valorConvertido.value = valor1 * 10;
-    }
   }
 
+  else if (from == "miliMetro"){
+    if (para == "centiMetro"){
+      valorConvertido.value = valor1/10;
+    }
+    if (para == "deciMetro"){
+      valorConvertido.value = valor1/100;
+    }
+    if (para == "metro"){
+      valorConvertido.value = valor1/1000;
+    }
+    if (para == "decaMetro"){
+      valorConvertido.value = valor1/10000;
+    }
+    if (para == "hectMetro"){
+      valorConvertido.value = valor1/100000;
+    }
+    if (para == "kiloMetro"){
+      valorConvertido.value = valor1/1000000;
+    }
+  }
+  else if (from == "centiMetro"){
+    if (para == "miliMetro"){
+      valorConvertido.value = valor1*10;
+    }
+    if (para == "deciMetro"){
+      valorConvertido.value = valor1/10;
+    }
+    if (para == "metro"){
+      valorConvertido.value = valor1/100;
+    }
+    if (para == "decaMetro"){
+      valorConvertido.value = valor1/1000;
+    }
+    if (para == "hectMetro"){
+      valorConvertido.value = valor1/10000;
+    }
+    if (para == "kiloMetro"){
+      valorConvertido.value = valor1/100000;
+    }
+  }
+  else if (from == "deciMetro"){
+    if (para == "miliMetro"){
+      valorConvertido.value = valor1*100;
+    }
+    if (para == "centiMetro"){
+      valorConvertido.value = valor1*10;
+    }
+    if (para == "metro"){
+      valorConvertido.value = valor1/10;
+    }
+    if (para == "decaMetro"){
+      valorConvertido.value = valor1/100;
+    }
+    if (para == "hectMetro"){
+      valorConvertido.value = valor1/1000;
+    }
+    if (para == "kiloMetro"){
+      valorConvertido.value = valor1/10000;
+    }
+  }
+  else if (from == "metro"){
+    if (para == "miliMetro"){
+      valorConvertido.value = valor1*1000;
+    }
+    if (para == "centiMetro"){
+      valorConvertido.value = valor1*100;
+    }
+    if (para == "deciMetro"){
+      valorConvertido.value = valor1*10;
+    }
+    if (para == "decaMetro"){
+      valorConvertido.value = valor1/10;
+    }
+    if (para == "hectMetro"){
+      valorConvertido.value = valor1/100;
+    }
+    if (para == "kiloMetro"){
+      valorConvertido.value = valor1/1000;
+    }
+  }
+  else if (from == "decaMetro"){
+    if (para == "miliMetro"){
+      valorConvertido.value = valor1*10000;
+    }
+    if (para == "centiMetro"){
+      valorConvertido.value = valor1*1000;
+    }
+    if (para == "deciMetro"){
+      valorConvertido.value = valor1*100;
+    }
+    if (para == "metro"){
+      valorConvertido.value = valor1*10;
+    }
+    if (para == "hectMetro"){
+      valorConvertido.value = valor1/10;
+    }
+    if (para == "kiloMetro"){
+      valorConvertido.value = valor1/100;
+    }
+  }
+  else if (from == "hectMetro"){
+    if (para == "miliMetro"){
+      valorConvertido.value = valor1*100000;
+    }
+    if (para == "centiMetro"){
+      valorConvertido.value = valor1*10000;
+    }
+    if (para == "deciMetro"){
+      valorConvertido.value = valor1*1000;
+    }
+    if (para == "metro"){
+      valorConvertido.value = valor1*100;
+    }
+    if (para == "decaMetro"){
+      valorConvertido.value = valor1*10;
+    }
+    if (para == "kiloMetro"){
+      valorConvertido.value = valor1/10;
+    }
+  }
+  else if (from == "kiloMetro"){
+    if (para == "miliMetro"){
+      valorConvertido.value = valor1*1000000;
+    }
+    if (para == "centiMetro"){
+      valorConvertido.value = valor1*100000;
+    }
+    if (para == "deciMetro"){
+      valorConvertido.value = valor1*10000;
+    }
+    if (para == "metro"){
+      valorConvertido.value = valor1*1000;
+    }
+    if (para == "decaMetro"){
+      valorConvertido.value = valor1*100;
+    }
+    if (para == "hectMetro"){
+      valorConvertido.value = valor1*10;
+    }
+  }
   e.preventDefault();
-
 }
 
-function limparCampos() {
-  document.getElementById('valor1').value = 0.0;
-  document.getElementById('valor2').value = 0.0;
+function limparCampos(){
+  document.getElementById('valor1').value = "";
+  document.getElementById('valor2').value = "";
 }
